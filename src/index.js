3348db99fb01aa7e97e67e3074698c2bec7783db
Kevin Vizconde <kcarlv@gmail.com>
2019-09-01 23:25:41 -0700

    updated added app.js and test files, also upddated package dependencies

--- src/index.js
+++ src/index.js
@@ -1,18 +1,8 @@
 /* CRUD : CREATE, READ, UPDATE, DELETE */
 /* POST, GET, PATCH, DELETE */
+const app = require('./app');
 
-const express = require('express');
-
-require('./db/mongoose');
-const userRouter = require('./routers/user');
-const taskRouter = require('./routers/task');
-
-const app = express();
 const port = process.env.PORT;
-
-app.use(express.json());
-app.use(userRouter);
-app.use(taskRouter);
 
 app.listen(port, () => {
   console.log(`Server is up on port ${port}`);
